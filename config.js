1df061aee48489d27faa116b5963ce8bf910ee2a
yup29-pro <ryashwanth429@gmail.com>
2026-03-13 06:41:16 +0530

    feat: add config.js via SpecDeploy

---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+  theme: {
+    light: {
+      background: '#ffffff',
+      text: '#000000'
+    },
+    dark: {
+      background: '#000000',
+      text: '#ffffff'
+    }
+  }
+};
+
+module.exports = config;
