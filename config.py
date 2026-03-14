ac42add7269a285915494aae49f0dad51f40dc6b
yup29-pro <ryashwanth429@gmail.com>
2026-03-14 11:45:21 +0530

    feat: add config.py via SpecDeploy

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+STRIPE_SECRET_KEY = 'stripe_secret_key'
+STRIPE_PUBLISHABLE_KEY = 'stripe_publishable_key'
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = 'email@gmail.com'
+MAIL_PASSWORD = 'email_password'
